5e0ca6a390b0dd12ce94c8c292491cdbbe0b7510
Andy Barilla <me@andybarilla.com>
2026-03-06 20:27:46 -0700

    remove containers zsh

--- dot_zshrc.d/containers/talos.zsh
+++
@@ -1,6 +0,0 @@
-export GOOGLE_CLOUD_PROJECT=development-37c5
-export GOOGLE_CLOUD_LOCATION=us-central1
-export GOOGLE_GENAI_USE_VERTEXAI=true 
-
-alias docker-up="distrobox-host-exec bash -c 'cd /var/home/andy/home/talos/titlevision-ai/devex && DOCKER_HOST=unix:///run/user/1000/podman/podman.sock docker-compose up -d'"
-
