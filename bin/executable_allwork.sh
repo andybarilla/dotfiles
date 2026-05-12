f10744d6039dba856d0e7d3c8485c12ebea639c5
Andy Barilla <me@andybarilla.com>
2026-05-12 13:56:14 -0600

    asst kde stuff

--- bin/executable_allwork.sh
+++ bin/executable_allwork.sh
@@ -2,10 +2,13 @@
 
 ~/bin/work-titlevision.sh
 ~/bin/work.sh kern ~/dev/printersrow/kern-app
+
 ~/bin/work.sh rook ~/dev/andybarilla/rook
-~/bin/work.sh jackdaw ~/dev/whattheflock/jackdaw
 ~/bin/work.sh janus ~/dev/andybarilla/janushc-dash
-~/bin/work.sh raven ~/dev/andybarilla/raven
 ~/bin/work.sh hq ~/dev/andybarilla/hq
 ~/bin/work.sh web-presence ~/dev/andybarilla/web-presence
 ~/bin/work.sh magpie ~/dev/andybarilla/magpie
+
+~/bin/work.sh jackdaw ~/dev/whattheflock/jackdaw
+~/bin/work.sh raven ~/dev/whattheflock/raven
+
